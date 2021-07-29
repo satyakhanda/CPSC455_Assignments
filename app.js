2719fa172fcc2b02a7319c27e1f104d81a951f39
Satyak Handa <45611870+satyakhanda@users.noreply.github.com>
2021-07-29 16:41:27 -0700

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', function (req, res) {
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.use(bodyParser.json())
