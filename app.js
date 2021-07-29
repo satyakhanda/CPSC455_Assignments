586122fef9554ab0a09bf74e5a8144eaa4aa9caa
Satyak Handa <45611870+satyakhanda@users.noreply.github.com>
2021-07-29 16:37:02 -0700

    Update app.js

--- app.js
+++ app.js
@@ -21,6 +21,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 app.use(bodyParser.json())
 
 let cards = [
